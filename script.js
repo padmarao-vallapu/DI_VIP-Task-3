fae957c257a912f071a936d356e8d3bde32fe5cc
padmarao-vallapu <vallapupadmaram7@gmail.com>
2023-09-16 10:54:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,174 @@
+const quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "javascript",
+        correct: "d",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+    {
+        question: "Javascript is an _______ language?",
+        a: "Object-Oriented",
+        b: "Object-Based",
+        c: "Procedural",
+        d: "none of the above",
+        correct: "a",
+    },
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        a: "var",
+        b: "let",
+        c: "Both a and b",
+        d: "none of the above",
+        correct: "c",
+    },
+    {
+        question: "Which of the following methods can be used to display data in some form using Javascript",   
+        a: "document.write()",
+        b: "console.log()",
+        c: "window.alert()",
+        d: "All of the above",
+        correct: "d",
+    },
+    {
+        question: "How can a datatype be declared to be a constant type?",
+        a: "const",
+        b: "let",
+        c: "var",
+        d: "constant",
+        correct: "a",
+    },
+    {
+        question: "What keyword is used to check whether a given property is valid or not?",
+        a: "in",
+        b: "is in",
+        c: "exists",
+        d: "lies",
+        correct: "a",
+    },
+    {
+        question: "  What is the use of the <noscript> tag in Javascript?",
+        a: "The contet are displayed by non-js browsers",
+        b: "Clears the all cocokis and cache",
+        c: "Both a & b",
+        d: "none of the above",
+        correct: "a",
+    },
+    {
+        question: "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+        a: "Null",
+        b: "Object",
+        c: "Interger",
+        d: "Undefined",
+        correct: "b",
+    },
+    {
+        question: "var a = true + true + true * 3"+
+        " print(a)",
+        a: "0",
+        b: "3",
+        c: "5",
+        d: "error",
+        correct: "c",
+    },
+    {
+        question: "What is the output of the following code snippet?"+
+
+       ' print(NaN === NaN)',
+        a: "True",
+        b: "False",
+        c: "Undefined",
+        d: "Error",
+        correct: "b",
+    },
+    {
+        question: "Which of the following are closures in Javascript?",
+        a: "Variables",
+        b: "Objects",
+        c: "Functions",
+        d: "All of the above",
+        correct: "d",
+    },
+    {
+        question: "Which of the following is not a Javascript framework?",
+        a: "Node",
+        b: "React",
+        c: "Vue",
+        d: "Cassandra",
+        correct: "d",
+    },
+];
+const quiz= document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+let currentQuiz = 0
+let score = 0
+loadQuiz()
+function loadQuiz() {
+    deselectAnswers()
+    const currentQuizData = quizData[currentQuiz]
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === quizData[currentQuiz].correct) {
+           score++
+       }
+       currentQuiz++
+       if(currentQuiz < quizData.length) {
+           loadQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+           <button onclick="location.reload()">RESTART</button>
+           `
+       }
+    }
+})
